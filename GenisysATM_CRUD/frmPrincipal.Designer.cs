ba9584d453517513ad05895d9e39ea61310c07a6
marcoselisorto <marcoselisorto@hotmail.com>
2018-11-14 19:57:50 -0600

    Creación del menú principal de opciones y codificación del boton salir

---
+++ GenisysATM_CRUD/frmPrincipal.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace GenisysATM_CRUD
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpOpciones = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnCuentas = new System.Windows.Forms.Button();
+            this.btnConfiguracion = new System.Windows.Forms.Button();
+            this.btnTarjetas = new System.Windows.Forms.Button();
+            this.btnServicios = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.grpOpciones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpOpciones
+            // 
+            this.grpOpciones.Controls.Add(this.btnSalir);
+            this.grpOpciones.Controls.Add(this.btnCuentas);
+            this.grpOpciones.Controls.Add(this.btnConfiguracion);
+            this.grpOpciones.Controls.Add(this.btnTarjetas);
+            this.grpOpciones.Controls.Add(this.btnServicios);
+            this.grpOpciones.Controls.Add(this.btnClientes);
+            this.grpOpciones.Controls.Add(this.lbltitulo);
+            this.grpOpciones.Location = new System.Drawing.Point(12, 12);
+            this.grpOpciones.Name = "grpOpciones";
+            this.grpOpciones.Size = new System.Drawing.Size(582, 201);
+            this.grpOpciones.TabIndex = 7;
+            this.grpOpciones.TabStop = false;
+            this.grpOpciones.Text = "Opciones Disponibles";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(460, 110);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(102, 58);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnCuentas
+            // 
+            this.btnCuentas.Location = new System.Drawing.Point(460, 32);
+            this.btnCuentas.Name = "btnCuentas";
+            this.btnCuentas.Size = new System.Drawing.Size(102, 58);
+            this.btnCuentas.TabIndex = 12;
+            this.btnCuentas.Text = "Cuentas";
+            this.btnCuentas.UseVisualStyleBackColor = true;
+            // 
+            // btnConfiguracion
+            // 
+            this.btnConfiguracion.Location = new System.Drawing.Point(311, 32);
+            this.btnConfiguracion.Name = "btnConfiguracion";
+            this.btnConfiguracion.Size = new System.Drawing.Size(102, 58);
+            this.btnConfiguracion.TabIndex = 11;
+            this.btnConfiguracion.Text = "Configuraciones";
+            this.btnConfiguracion.UseVisualStyleBackColor = true;
+            // 
+            // btnTarjetas
+            // 
+            this.btnTarjetas.Location = new System.Drawing.Point(166, 32);
+            this.btnTarjetas.Name = "btnTarjetas";
+            this.btnTarjetas.Size = new System.Drawing.Size(112, 58);
+            this.btnTarjetas.TabIndex = 10;
+            this.btnTarjetas.Text = "Tarjetas de \r\n  Crédito";
+            this.btnTarjetas.UseVisualStyleBackColor = true;
+            // 
+            // btnServicios
+            // 
+            this.btnServicios.Location = new System.Drawing.Point(20, 110);
+            this.btnServicios.Name = "btnServicios";
+            this.btnServicios.Size = new System.Drawing.Size(102, 58);
+            this.btnServicios.TabIndex = 9;
+            this.btnServicios.Text = "Servicios";
+            this.btnServicios.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Location = new System.Drawing.Point(20, 32);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(102, 58);
+            this.btnClientes.TabIndex = 8;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(226, 144);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(127, 24);
+            this.lbltitulo.TabIndex = 7;
+            this.lbltitulo.Text = "Genisys ATM";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 226);
+            this.Controls.Add(this.grpOpciones);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cajeros Automáticos Genisys ATM";
+            this.grpOpciones.ResumeLayout(false);
+            this.grpOpciones.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpOpciones;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCuentas;
+        private System.Windows.Forms.Button btnConfiguracion;
+        private System.Windows.Forms.Button btnTarjetas;
+        private System.Windows.Forms.Button btnServicios;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Label lbltitulo;
+    }
+}
+
---
+++ GenisysATM_CRUD/frmPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenisysATM_CRUD
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Cierra el menú principal y cierra la aplicación
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult resp = MessageBox.Show("¿Está seguro que desa salir del programa?", "Control de cajeros automáticos", MessageBoxButtons.YesNo);
+
+            if (resp == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
