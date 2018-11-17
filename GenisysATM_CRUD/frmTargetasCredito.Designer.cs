b1161ac32b6e8cf46e6a0da56b6f18750af97910
marcoselisorto <marcoselisorto@hotmail.com>
2018-11-17 12:07:28 -0600

    Creación del formulario CRUD tarjetas de Crédito

---
+++ GenisysATM_CRUD/frmTargetasCredito.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace GenisysATM_CRUD
+{
+    partial class frmTargetasCredito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpOperaciones = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtlimite = new System.Windows.Forms.TextBox();
+            this.txtmonto = new System.Windows.Forms.TextBox();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblIdentidad = new System.Windows.Forms.Label();
+            this.lstClientes = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.grpOperaciones.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpOperaciones
+            // 
+            this.grpOperaciones.Controls.Add(this.btnSalir);
+            this.grpOperaciones.Controls.Add(this.btnEliminar);
+            this.grpOperaciones.Controls.Add(this.btnEditar);
+            this.grpOperaciones.Controls.Add(this.btnAgregar);
+            this.grpOperaciones.Location = new System.Drawing.Point(60, 227);
+            this.grpOperaciones.Name = "grpOperaciones";
+            this.grpOperaciones.Size = new System.Drawing.Size(499, 57);
+            this.grpOperaciones.TabIndex = 29;
+            this.grpOperaciones.TabStop = false;
+            this.grpOperaciones.Text = "Operaciones";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(6, 19);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(102, 30);
+            this.btnSalir.TabIndex = 16;
+            this.btnSalir.Text = "Regresar";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(377, 19);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(102, 30);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(269, 19);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(102, 30);
+            this.btnEditar.TabIndex = 1;
+            this.btnEditar.Text = "Actualizar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(158, 19);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(102, 30);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Guardar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtlimite
+            // 
+            this.txtlimite.Location = new System.Drawing.Point(206, 80);
+            this.txtlimite.Name = "txtlimite";
+            this.txtlimite.Size = new System.Drawing.Size(211, 20);
+            this.txtlimite.TabIndex = 25;
+            // 
+            // txtmonto
+            // 
+            this.txtmonto.Location = new System.Drawing.Point(206, 47);
+            this.txtmonto.Name = "txtmonto";
+            this.txtmonto.Size = new System.Drawing.Size(211, 20);
+            this.txtmonto.TabIndex = 24;
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(154, 135);
+            this.txtdescripcion.Multiline = true;
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(263, 86);
+            this.txtdescripcion.TabIndex = 23;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(151, 80);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(43, 16);
+            this.lblApellido.TabIndex = 19;
+            this.lblApellido.Text = "Límite";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(151, 47);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(48, 16);
+            this.lblNombre.TabIndex = 18;
+            this.lblNombre.Text = "Monto:";
+            // 
+            // lblIdentidad
+            // 
+            this.lblIdentidad.AutoSize = true;
+            this.lblIdentidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdentidad.Location = new System.Drawing.Point(151, 116);
+            this.lblIdentidad.Name = "lblIdentidad";
+            this.lblIdentidad.Size = new System.Drawing.Size(83, 16);
+            this.lblIdentidad.TabIndex = 17;
+            this.lblIdentidad.Text = "Descripción:";
+            // 
+            // lstClientes
+            // 
+            this.lstClientes.FormattingEnabled = true;
+            this.lstClientes.Location = new System.Drawing.Point(12, 47);
+            this.lstClientes.Name = "lstClientes";
+            this.lstClientes.Size = new System.Drawing.Size(136, 173);
+            this.lstClientes.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Módulo de Clientes";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(423, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 199);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tarjetas asignadas al cliente";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(19, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(144, 160);
+            this.listBox1.TabIndex = 17;
+            // 
+            // frmTargetasCredito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 296);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grpOperaciones);
+            this.Controls.Add(this.txtlimite);
+            this.Controls.Add(this.txtmonto);
+            this.Controls.Add(this.txtdescripcion);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblIdentidad);
+            this.Controls.Add(this.lstClientes);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTargetasCredito";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Control de Cajeros Automáticos GenisysATM";
+            this.grpOperaciones.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpOperaciones;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtlimite;
+        private System.Windows.Forms.TextBox txtmonto;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblIdentidad;
+        private System.Windows.Forms.ListBox lstClientes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
