306583490b7004c8fd67b73d5ff167e09bb0f637
marcoselisorto <marcoselisorto@hotmail.com>
2018-11-17 18:11:57 -0600

    Creación del formulario CRUN de CuentaClietes

---
+++ GenisysATM_CRUD/frmCuentaCliente.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace GenisysATM_CRUD
+{
+    partial class frmCuentaCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpOperaciones = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtPin = new System.Windows.Forms.TextBox();
+            this.txtSaldo = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblIdentidad = new System.Windows.Forms.Label();
+            this.lstClientes = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpOperaciones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpOperaciones
+            // 
+            this.grpOperaciones.Controls.Add(this.btnSalir);
+            this.grpOperaciones.Controls.Add(this.btnEliminar);
+            this.grpOperaciones.Controls.Add(this.btnEditar);
+            this.grpOperaciones.Controls.Add(this.btnAgregar);
+            this.grpOperaciones.Location = new System.Drawing.Point(12, 249);
+            this.grpOperaciones.Name = "grpOperaciones";
+            this.grpOperaciones.Size = new System.Drawing.Size(499, 57);
+            this.grpOperaciones.TabIndex = 29;
+            this.grpOperaciones.TabStop = false;
+            this.grpOperaciones.Text = "Operaciones";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(6, 19);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(102, 30);
+            this.btnSalir.TabIndex = 16;
+            this.btnSalir.Text = "Regresar";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(377, 19);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(102, 30);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(269, 19);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(102, 30);
+            this.btnEditar.TabIndex = 1;
+            this.btnEditar.Text = "Actualizar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(158, 19);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(102, 30);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Guardar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtPin
+            // 
+            this.txtPin.Location = new System.Drawing.Point(280, 142);
+            this.txtPin.Name = "txtPin";
+            this.txtPin.Size = new System.Drawing.Size(211, 20);
+            this.txtPin.TabIndex = 24;
+            // 
+            // txtSaldo
+            // 
+            this.txtSaldo.Location = new System.Drawing.Point(280, 109);
+            this.txtSaldo.Name = "txtSaldo";
+            this.txtSaldo.Size = new System.Drawing.Size(211, 20);
+            this.txtSaldo.TabIndex = 23;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(206, 142);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(27, 16);
+            this.lblNombre.TabIndex = 18;
+            this.lblNombre.Text = "Pin";
+            // 
+            // lblIdentidad
+            // 
+            this.lblIdentidad.AutoSize = true;
+            this.lblIdentidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdentidad.Location = new System.Drawing.Point(206, 109);
+            this.lblIdentidad.Name = "lblIdentidad";
+            this.lblIdentidad.Size = new System.Drawing.Size(45, 16);
+            this.lblIdentidad.TabIndex = 17;
+            this.lblIdentidad.Text = "saldo:";
+            // 
+            // lstClientes
+            // 
+            this.lstClientes.FormattingEnabled = true;
+            this.lstClientes.Location = new System.Drawing.Point(12, 46);
+            this.lstClientes.Name = "lstClientes";
+            this.lstClientes.Size = new System.Drawing.Size(183, 199);
+            this.lstClientes.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Módulo de Clientes y Cuentas";
+            // 
+            // frmCuentaCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 316);
+            this.Controls.Add(this.grpOperaciones);
+            this.Controls.Add(this.txtPin);
+            this.Controls.Add(this.txtSaldo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblIdentidad);
+            this.Controls.Add(this.lstClientes);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCuentaCliente";
+            this.Text = "Control de Cajeros Automáticos GenisysATM";
+            this.grpOperaciones.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpOperaciones;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtPin;
+        private System.Windows.Forms.TextBox txtSaldo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblIdentidad;
+        private System.Windows.Forms.ListBox lstClientes;
+        private System.Windows.Forms.Label label1;
+    }
+}
